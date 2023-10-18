f8bbb8353c4c44619a959e94cf78180b7e58eb78
Jaypal Mudaliyar <jaypal.m@Jaypal-Mudaliyar-R5X4L7RJFG.local>
2023-10-18 20:35:21 +0530

    backend/feat/bap: error types and instances for conecpt of city

--- lib/mobility-core/src/Kernel/Types/Beckn/City.hs
+++ lib/mobility-core/src/Kernel/Types/Beckn/City.hs
@@ -19,16 +19,19 @@ module Kernel.Types.Beckn.City (City (..)) where
 
 import Data.Aeson
 import Data.Aeson.Types
+import Data.Char (toLower)
 import Data.OpenApi hiding (Example)
+import qualified Data.Text as T
 import EulerHS.Prelude
 import Kernel.Beam.Lib.UtilsTH (mkBeamInstancesForEnum)
 import Kernel.Storage.Esqueleto
 import Kernel.Utils.GenericPretty
+import Servant.API (FromHttpApiData (..), ToHttpApiData (..))
 
 derivePersistField "City"
 
 data City = Bangalore | Kolkata | Paris | Kochi | AnyCity
-  deriving (Eq, Generic, Show, Read, ToSchema, Ord)
+  deriving (Eq, Generic, Show, Read, ToSchema, Ord, ToParamSchema)
   deriving (PrettyShow) via Showable City
 
 $(mkBeamInstancesForEnum ''City)
@@ -52,3 +55,26 @@ instance ToJSON City where
   toJSON Paris = String "std:001"
   toJSON Kochi = String "std:484"
   toJSON AnyCity = String "*"
+
+instance FromHttpApiData City where
+  parseUrlPiece a =
+    let lower = map toLower $ T.unpack a
+     in parseLowerCaseCity lower
+    where
+      parseLowerCaseCity "std:080" = Right Bangalore
+      parseLowerCaseCity "bangalore" = Right Bangalore
+      parseLowerCaseCity "std:033" = Right Kolkata
+      parseLowerCaseCity "kolkata" = Right Kolkata
+      parseLowerCaseCity "std:001" = Right Paris
+      parseLowerCaseCity "paris" = Right Paris
+      parseLowerCaseCity "std:484" = Right Kochi
+      parseLowerCaseCity "kochi" = Right Kochi
+      parseLowerCaseCity "*" = Right AnyCity
+      parseLowerCaseCity city = Left . T.pack $ ("ParseFail: Unable to parse city: " <> city)
+
+instance ToHttpApiData City where
+  toUrlPiece Bangalore = "std:080"
+  toUrlPiece Kolkata = "std:033"
+  toUrlPiece Paris = "std:001"
+  toUrlPiece Kochi = "std:484"
+  toUrlPiece AnyCity = "*"
